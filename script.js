0f31041ea4f1411fa75f59ff8fc9c939302ba7f7
lowchain3071 <lowchain3071@gmail.com>
2023-09-11 19:02:45 +0000

    more edits

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const canvas = document.getElementById("canvas1");
 const ctx = canvas.getContext("2d");
 canvas.width = 640;
 canvas.height = 360;
+console.log(ctx);
 
 //event listeners
 let keys = [];
@@ -26,12 +27,17 @@ xhr.responseType = "text";
 
 //xhr onload event
 xhr.onload = event => {
-  if(xhr.status = 200){
-    JSON.parse(xhr.responseText);
-  }
+  objects = JSON.parse(xhr.responseText);
+  let player = objects.player;
+console.log(player)
 } 
 xhr.send(); //send xhr request
 
+let player = objects.player;
+console.log(player);
+
 function animate(){
+
+  
   requestAnimationFrame(animate);
 }
