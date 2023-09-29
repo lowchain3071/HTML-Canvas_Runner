7e355dc2227ec0b3c1cb0152ac172b93fe044dec
lowchain3071 <lowchain3071@gmail.com>
2023-09-29 21:44:09 +0000

    clearing pre-break

--- control.js
+++
@@ -1,5 +0,0 @@
-import { Game } from "./main.js"
-let game = new Game();
-function animate(){
-  requestAnimationFrame(animate);
-} 
--- script.js
+++
@@ -1,8 +0,0 @@
-const canvas = document.getElementById("canvas1");
-const ctx = canvas.getContext("2d");
-canvas.width = 640;
-canvas.height = 360;
-
-function animate(){
-  requestAnimationFrame(animate);
-}
